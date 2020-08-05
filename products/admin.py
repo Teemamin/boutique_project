a0f00c7d53a96f393bbdf91589869e1821cb26ea
Fatima Aminu <teemamin@yahoo.com>
2020-08-05 09:20:21 +0000

    customized admin

--- products/admin.py
+++ products/admin.py
@@ -4,5 +4,30 @@ from .models import Product, Category
 # Register your models here.
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+# the below two classes exteds the builtin admin
+# and usuing the list_display frm django
+# it tells the admin which fields to disply
+# rembr to always register the classes
+# nxt to their respective models
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'Categories'
+        # added the class meta to make d category name into categories for plural
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
