1ad2a7e8d356890cc1e55ea10775db20be2f9c8e
Fatima Aminu <teemamin@yahoo.com>
2020-08-06 11:20:00 +0000

    sort select box added

--- products/views.py
+++ products/views.py
@@ -14,9 +14,8 @@ def all_products(request):
     # the page,cos at this stage error is empty
     categories = None
     sort = None
-    direction = None
-    
-
+    direction = None   
+ 
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -24,6 +23,10 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -48,11 +51,12 @@ def all_products(request):
             products = products.filter(queries)
    
     current_sorting = f'{sort}_{direction}'
-    
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'products/products.html', context)
 
