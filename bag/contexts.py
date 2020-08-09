8a2b63e691397ccc4c85863bc1a7951c6667612e
Fatima Aminu <teemamin@yahoo.com>
2020-08-09 10:09:56 +0000

    remove and delete added to bag template with js functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,15 +11,31 @@ def bag_contents(request):
     # chk if var bag exist in request session,if it doesn't add
     bag = request.session.get('bag', {})
     # for is looping through the var bag request session
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # If d instance is an integer then we know the item data is
+        #  just the quantity.Otherwise we know it's a dictionary and
+        #  we need to handle it wit else.
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -21,14 +21,37 @@ def add_to_bag(request, item_id):
     #  in the session.we can keep track of thr purchase per session
     #  it chks to see if the var bag already exisit in the session
     #  if not it will initialze it as an empty dict
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
     # If the item is already in the bag in other words
     # if there's already a key in the bag dictionary matching this product id.
     # Then I'll increment its quantity accordingly.else add the item n quantity
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    # if there is size
+    if size:
+        # If the item is already in the bag.
+        if item_id in list(bag.keys()):
+            # Then we need to check if another item of the same id and
+            #  same size already exists.
+            if size in bag[item_id]['items_by_size'].keys():
+                # if so increment the quantity for that size
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                #  otherwise just set it equal to the quantity.
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # If the items not already in the bag we just need to add it.
+            #  But we're actually going to do it as a dictionary with a key
+            #  of items_by_size.Since we may have multiple items with
+            #  this item id. But different sizes.
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        # if there's no size we run this logic.
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
     # this inputs the bag var into the session var
     request.session['bag'] = bag
     #  print(request.session['bag'])
