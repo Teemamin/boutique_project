9d635d6083ce7e209ebb2d0a3ebf0617cbb9421e
Fatima Aminu <teemamin@yahoo.com>
2020-08-13 15:06:39 +0000

    a view to handle saving user info started

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,7 +5,8 @@
     https://stripe.com/docs/stripe-js
 */
 /**
- * gets the stripe public key.And client secret from the template using  jQuery.
+ * gets the stripe public key and client secret from the template using  jQuery.
+ * see notes for further explanation on how to get cleintsecret
  * and removes(slice) the qotation marks at the beginin n end 
  */
 var stripePublic_key = $('#id_stripe_public_key').text().slice(1, -1);
@@ -66,7 +67,31 @@ form.addEventListener('submit', function(ev) {
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
-        }
+            billing_details: { //see note for further explanations
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
+                email: $.trim(form.email.value),
+                address:{
+                    line1: $.trim(form.street_address1.value),
+                    line2: $.trim(form.street_address2.value),
+                    city: $.trim(form.town_or_city.value),
+                    country: $.trim(form.country.value),
+                    state: $.trim(form.county.value),
+                }
+            }
+        },
+        shipping: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            address: {
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                postal_code: $.trim(form.postcode.value),
+                state: $.trim(form.county.value),
+            }
+        },
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
